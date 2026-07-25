f2e34a7f3af4d1554c2046de2dc1f92ce5aa3ee4
kacpet <70849469+kacpet@users.noreply.github.com>
2026-07-26 01:15:56 +0200

    Updates

--- assets/MobileHome-C8RbZGjo.js
+++ assets/MobileHome-C8RbZGjo.js
@@ -1,17 +1,20 @@
-import{t as e}from"./index-lKhfFXvt.js";import{t}from"./react-v9gTOvsp.js";import{t as n}from"./arrow-B3jBI4JY.js";import{t as r}from"./TabletSlider-nFq-XXom.js";var i=e(),a={hidden:{},visible:{transition:{staggerChildren:.2}}},o={hidden:{opacity:0,y:30},visible:{opacity:1,y:0,transition:{duration:.7}}};function s(){return(0,i.jsxs)(`div`,{id:`home`,className:`\r
-        min-h-dvh\r
-        w-full\r
-        bg-[#DCC1AB]\r
+import{t as e}from"./index-B34czh8J.js";import{t}from"./react-CU1olgwU.js";import{t as n}from"./arrow-B3jBI4JY.js";import{t as r}from"./TabletSlider-49sBVA_T.js";var i=e(),a={hidden:{},visible:{transition:{staggerChildren:.2}}},o={hidden:{opacity:0,y:30},visible:{opacity:1,y:0,transition:{duration:.7}}};function s(){return(0,i.jsxs)(`div`,{id:`home`,className:`\r
+        relative\r
         flex\r
+        h-screen\r
+        w-full\r
         flex-col\r
+        overflow-hidden\r
+        bg-[#DCC1AB]\r
+        pt-24\r
       `,children:[(0,i.jsx)(t.div,{variants:a,initial:`hidden`,animate:`visible`,className:`\r
-          flex-1\r
           flex\r
+          flex-1\r
           items-center\r
+          justify-center\r
           px-9\r
-          pt-14\r
-          pb-8\r
-        `,children:(0,i.jsxs)(`div`,{className:`flex w-full flex-col`,children:[(0,i.jsxs)(t.h1,{variants:o,className:`\r
+          py-8\r
+        `,children:(0,i.jsxs)(`div`,{className:`flex w-full max-w-xl flex-col`,children:[(0,i.jsxs)(t.h1,{variants:o,className:`\r
               text-3xl\r
               leading-tight\r
               font-medium\r
@@ -20,13 +23,17 @@ import{t as e}from"./index-lKhfFXvt.js";import{t}from"./react-v9gTOvsp.js";impor
               text-sm\r
               leading-relaxed\r
               font-inter\r
+              font-normal\r
             `,children:`Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji. Oferujemy kompleksowy zakres usług z indywidualnym podejściem do każdego projektu.`}),(0,i.jsxs)(t.div,{variants:o,className:`\r
               mt-6\r
               flex\r
-              justify-center\r
-              gap-4\r
+              flex-col\r
+              items-center\r
+              gap-3\r
             `,children:[(0,i.jsx)(`a`,{href:`#contact`,className:`\r
                 flex\r
+                w-[60%]\r
+                min-w-[250px]\r
                 items-center\r
                 justify-center\r
                 rounded-full\r
@@ -35,11 +42,16 @@ import{t as e}from"./index-lKhfFXvt.js";import{t}from"./react-v9gTOvsp.js";impor
                 py-2.5\r
                 text-sm\r
                 text-[#DCC1AB]\r
-                transition\r
-                duration-300\r
+                font-inter\r
+                transition-transform\r
+                duration-500\r
                 hover:scale-105\r
+                hover:bg-green-800\r
+                hover:shadow-[0_2px_10px_rgba(20,83,45,0.15)]\r
               `,children:`Skontaktuj się z nami`}),(0,i.jsxs)(`a`,{href:`#projects`,className:`\r
                 flex\r
+                w-[60%]\r
+                min-w-[250px]\r
                 items-center\r
                 justify-center\r
                 rounded-full\r
@@ -49,10 +61,9 @@ import{t as e}from"./index-lKhfFXvt.js";import{t}from"./react-v9gTOvsp.js";impor
                 py-2.5\r
                 text-sm\r
                 text-[#1B5B31]\r
-                transition\r
-                duration-300\r
+                font-inter\r
+                transition-transform\r
+                duration-500\r
                 hover:scale-105\r
-              `,children:[(0,i.jsx)(`span`,{className:`mr-2`,children:`Zobacz nasze realizacje`}),(0,i.jsx)(`img`,{src:n,alt:`strzałka`,className:`w-4`})]})]})]})}),(0,i.jsx)(`div`,{className:`\r
-          mt-auto\r
-          w-full\r
-        `,children:(0,i.jsx)(r,{})})]})}export{s as default};
+                hover:shadow-[0_2_10px_rgba(20,83,45,0.15)]\r
+              `,children:[(0,i.jsx)(`span`,{className:`mr-2`,children:`Zobacz nasze realizacje`}),(0,i.jsx)(`img`,{src:n,alt:`strzałka`,className:`w-4`})]})]})]})}),(0,i.jsx)(`div`,{className:`mt-auto w-full`,children:(0,i.jsx)(r,{})})]})}export{s as default};
